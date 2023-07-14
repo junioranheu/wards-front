c8e00cc70fdf0011bfb2c7e525ba67ab00093486
junioranheu <junioranheu@gmail.com>
2023-07-13 23:39:12 -0300

    estruturação inicial do projeto

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -3,7 +3,16 @@ import Styles from './index.module.scss';
 export default function Navbar() {
     return (
         <nav className={Styles.navbar}>
-            <h1>Navbar</h1>
+            <div className={Styles.esquerda}>
+                <a>Logo</a>
+                <a>Sobre</a>
+            </div>
+
+            <div className={Styles.direita}>
+                <a>Icone de busca</a>
+                <a>Entrar</a>
+                <a>Criar conta</a>
+            </div>
         </nav>
     )
 }
