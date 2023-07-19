917dd3cf92f750681fc3d05280362407396d30fa
junioranheu <junioranheu@gmail.com>
2023-07-18 23:23:43 -0300

    refactor Navbar

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,28 +1,27 @@
-import Botao from '@/components/botao';
-import IconeLupa from '@/components/icones/lupa';
 import ModalLayout from '@/components/modal/_modal.layout';
 import ModalWrapper from '@/components/modal/_modal.wrapper';
 import ModalAuth from '@/components/modal/modal.auth';
 import useUsuarioContext from '@/hooks/context/useUsuarioContext';
+import useWindowSize from '@/hooks/useWindowSize';
 import CONSTS_USUARIOS from '@/utils/api/consts/usuarios';
 import { Fetch } from '@/utils/api/fetch';
 import CONSTS_EMOJIS from '@/utils/consts/emojis';
 import CONSTS_MODAL from '@/utils/consts/modal.tamanho';
 import CONSTS_SISTEMA from '@/utils/consts/sistema';
-import CONSTS_TELAS from '@/utils/consts/telas';
 import { Auth } from '@/utils/context/usuarioContext';
-import { CONST_NANUM } from '@/utils/fonts/fonts';
 import { Aviso } from '@/utils/functions/aviso';
 import gerarStringAleatoria from '@/utils/functions/gerar.stringAleatoria';
 import iUsuario from '@/utils/types/iUsuario';
 import iUsuarioContext from '@/utils/types/iUsuario.context';
-import Link from 'next/link';
 import { Fragment, useState } from 'react';
-import Styles from './index.module.scss';
+import NavbarFull from './navbar-full';
+import NavbarSmall from './navbar-small';
 
 export default function Navbar() {
 
     const [isAuth, setIsAuth] = useUsuarioContext();
+    const windowSize = useWindowSize();
+    const mediaQueryLimite = 1025;
 
     const [isModalLoginOpen, setIsModalLoginOpen] = useState<boolean>(false);
     const [isModalCriarContaOpen, setIsModalCriarContaOpen] = useState<boolean>(false);
@@ -158,42 +157,23 @@ export default function Navbar() {
                 </ModalLayout>
             </ModalWrapper>
 
-            <nav className={Styles.navbar}>
-                <div className={Styles.esquerda}>
-                    <Link className={`${CONST_NANUM.className} ${Styles.logo} wavy`} href={CONSTS_TELAS.INDEX}>{CONSTS_SISTEMA.NOME_SISTEMA}</Link>
-                    <Link href={CONSTS_TELAS.SOBRE}>Sobre</Link>
-                </div>
-
-                <div className={Styles.direita}>
-                    <IconeLupa
-                        escala={0.75}
-                        url={null}
-                        isNovaAba={false}
-                        handleFuncao={() => null}
+            {
+                windowSize.width <= mediaQueryLimite ? (
+                    <NavbarSmall
+                        isAuth={isAuth}
+                        setIsModalLoginOpen={setIsModalLoginOpen}
+                        setIsModalCriarContaOpen={setIsModalCriarContaOpen}
+                        handleLogoff={handleLogoff}
                     />
-
-                    {
-                        !isAuth ? (
-                            <Fragment>
-                                <a onClick={() => setIsModalLoginOpen(true)}>Entrar</a>
-
-                                <Botao
-                                    texto='Criar conta'
-                                    url={null}
-                                    isNovaAba={true}
-                                    handleFuncao={() => setIsModalCriarContaOpen(true)}
-                                    Svg={null}
-                                    refBtn={null}
-                                    isEnabled={true}
-                                    isPequeno={true}
-                                />
-                            </Fragment>
-                        ) : (
-                            <a onClick={() => handleLogoff()}>Sair</a>
-                        )
-                    }
-                </div>
-            </nav>
+                ) : (
+                    <NavbarFull
+                        isAuth={isAuth}
+                        setIsModalLoginOpen={setIsModalLoginOpen}
+                        setIsModalCriarContaOpen={setIsModalCriarContaOpen}
+                        handleLogoff={handleLogoff}
+                    />
+                )
+            }
         </Fragment>
     )
 }
---
+++ components/navbar/navbar-full/index.tsx
@@ -0,0 +1,56 @@
+import Botao from '@/components/botao';
+import IconeLupa from '@/components/icones/lupa';
+import CONSTS_SISTEMA from '@/utils/consts/sistema';
+import CONSTS_TELAS from '@/utils/consts/telas';
+import { CONST_NANUM } from '@/utils/fonts/fonts';
+import Link from 'next/link';
+import { Dispatch, Fragment, SetStateAction } from 'react';
+import Styles from '../index.module.scss';
+
+interface iParametros {
+    isAuth: boolean;
+    setIsModalLoginOpen: Dispatch<SetStateAction<boolean>>;
+    setIsModalCriarContaOpen: Dispatch<SetStateAction<boolean>>;
+    handleLogoff: () => void;
+}
+
+export default function NavbarFull({ isAuth, setIsModalLoginOpen, setIsModalCriarContaOpen, handleLogoff }: iParametros) {
+    return (
+        <nav className={Styles.navbar}>
+            <div className={Styles.esquerda}>
+                <Link className={`${CONST_NANUM.className} ${Styles.logo} wavy`} href={CONSTS_TELAS.INDEX}>{CONSTS_SISTEMA.NOME_SISTEMA}</Link>
+                <Link href={CONSTS_TELAS.SOBRE}>Sobre</Link>
+            </div>
+
+            <div className={Styles.direita}>
+                <IconeLupa
+                    escala={0.75}
+                    url={null}
+                    isNovaAba={false}
+                    handleFuncao={() => null}
+                />
+
+                {
+                    !isAuth ? (
+                        <Fragment>
+                            <a onClick={() => setIsModalLoginOpen(true)}>Entrar</a>
+
+                            <Botao
+                                texto='Criar conta'
+                                url={null}
+                                isNovaAba={true}
+                                handleFuncao={() => setIsModalCriarContaOpen(true)}
+                                Svg={null}
+                                refBtn={null}
+                                isEnabled={true}
+                                isPequeno={true}
+                            />
+                        </Fragment>
+                    ) : (
+                        <a onClick={() => handleLogoff()}>Sair</a>
+                    )
+                }
+            </div>
+        </nav>
+    )
+}
