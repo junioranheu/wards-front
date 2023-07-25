8ccc081392f71567eb3835901fbf481fdc9032d7
junioranheu <junioranheu@gmail.com>
2023-07-24 22:24:07 -0300

    ajustes gerais

--- components/navbar/navbar-full/index.tsx
+++ components/navbar/navbar-full/index.tsx
@@ -1,8 +1,6 @@
 import Botao from '@/components/botao';
 import IconeEntrar from '@/components/icones/entrar';
-import IconeInfo from '@/components/icones/info';
 import IconeLupa from '@/components/icones/lupa';
-import IconeSorte from '@/components/icones/sorte';
 import CONSTS_SISTEMA from '@/utils/consts/sistema';
 import CONSTS_TELAS from '@/utils/consts/telas';
 import { CONST_NANUM } from '@/utils/fonts/fonts';
@@ -23,8 +21,11 @@ export default function NavbarFull({ isAuth, setIsModalLoginOpen, setIsModalCria
         <nav className={Styles.navbar}>
             <div className={Styles.esquerda}>
                 <Link className={`${CONST_NANUM.className} ${Styles.logo} wavy`} href={CONSTS_TELAS.INDEX}>{CONSTS_SISTEMA.NOME_SISTEMA}</Link>
-                <IconeInfo escala={0.65} url={CONSTS_TELAS.SOBRE} isNovaAba={false} handleFuncao={() => null} placeholder='Sobre' />
-                <IconeSorte escala={0.75} url={null} isNovaAba={false} handleFuncao={() => redirecionarWardAleatoria()} placeholder='Estou com sorte' />
+
+                <Link href={CONSTS_TELAS.SOBRE}>Sobre</Link>
+                <a onClick={() => redirecionarWardAleatoria()}>Estou com sorte</a>
+                {/* <IconeInfo escala={0.65} url={CONSTS_TELAS.SOBRE} isNovaAba={false} handleFuncao={() => null} placeholder='Sobre' />
+                <IconeSorte escala={0.75} url={null} isNovaAba={false} handleFuncao={() => redirecionarWardAleatoria()} placeholder='Estou com sorte' /> */}
             </div>
 
             <div className={Styles.direita}>
