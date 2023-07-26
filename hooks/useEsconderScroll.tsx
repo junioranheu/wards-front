7d870637654ae928724719668501fd9c86049f59
junioranheu <junioranheu@gmail.com>
2023-07-26 18:58:00 -0300

    teste handleDesabilitarScrollMobile

--- hooks/useEsconderScroll.tsx
+++ hooks/useEsconderScroll.tsx
@@ -19,10 +19,21 @@ export default function useEsconderScroll() {
 
     useEffect(() => {
         document.querySelector('html')!.style.overflow = 'hidden';
-        handleDesabilitarScrollMobile();
+
+        const isIphone = /iPhone|iPod/.test(navigator.userAgent);
+        const isAndroid = /Android/.test(navigator.userAgent);
+        const preventScrollMobile = (event: any) => event.preventDefault();
+
+        alert(isIphone);
+        alert(isAndroid);
+
+        if (isIphone || isAndroid) {
+            document.body.addEventListener('touchmove', preventScrollMobile, { passive: false });
+        }
 
         return () => {
             document.querySelector('html')!.style.overflow = 'auto';
+            document.body.removeEventListener('touchmove', preventScrollMobile);
         };
     }, []);
 
