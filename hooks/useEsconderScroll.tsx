8c70a63f6a49e4461405c36e6aa659ac6aba9797
junioranheu <junioranheu@gmail.com>
2023-07-26 18:59:48 -0300

    refactor useEsconderScroll

--- hooks/useEsconderScroll.tsx
+++ hooks/useEsconderScroll.tsx
@@ -1,31 +1,12 @@
 import { useEffect } from 'react';
 
 export default function useEsconderScroll() {
-
-    function handleDesabilitarScrollMobile() {
-        const isIphone = /iPhone|iPod/.test(navigator.userAgent);
-        const isAndroid = /Android/.test(navigator.userAgent);
-        const body = document.body;
-
-        if (isIphone || isAndroid) {
-            const preventScroll = (event: any) => event.preventDefault();
-            body.addEventListener('touchmove', preventScroll, { passive: false });
-
-            return () => {
-                body.removeEventListener('touchmove', preventScroll);
-            };
-        }
-    };
-
     useEffect(() => {
         document.querySelector('html')!.style.overflow = 'hidden';
 
         const isIphone = /iPhone|iPod/.test(navigator.userAgent);
         const isAndroid = /Android/.test(navigator.userAgent);
         const preventScrollMobile = (event: any) => event.preventDefault();
-
-        alert(isIphone);
-        alert(isAndroid);
 
         if (isIphone || isAndroid) {
             document.body.addEventListener('touchmove', preventScrollMobile, { passive: false });
