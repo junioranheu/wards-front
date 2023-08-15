4e8e7144c390913677e0a65e69f5386b0ddb211a
junioranheu <junioranheu@gmail.com>
2023-08-14 22:43:32 -0300

    fix normalizarSanitizeHTML

--- utils/functions/normalizar.sanitizeHTML.ts
+++ utils/functions/normalizar.sanitizeHTML.ts
@@ -1,5 +1,10 @@
 import sanitizeHtml from 'sanitize-html';
 
 export default function normalizarSanitizeHTML(html: string): string {
-    return sanitizeHtml(html);
+    return sanitizeHtml(html, {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
+        allowedAttributes: { 'img': ['src'] },
+        // allowedSchemes: ['data', 'http', 'https']
+        allowedSchemes: ['data', 'https']
+    });
 }
