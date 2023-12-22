6f8850a7cb76a6031c7bfb507645330d121dbab0
Junior <junioranheu@gmail.com>
2023-12-22 19:15:13 -0300

    update useUsuarioContext.ts

--- hooks/context/useUsuarioContext.ts
+++ hooks/context/useUsuarioContext.ts
@@ -2,10 +2,8 @@ import { UsuarioContext } from '@/utils/context/usuarioContext';
 import { Dispatch, SetStateAction, useContext } from 'react';
 
 export default function useUsuarioContext(): [boolean, Dispatch<SetStateAction<boolean>>] {
-
-    const usuarioContext = useContext(UsuarioContext);
-    const [isAuth, setIsAuth] = [usuarioContext?.isAuthContext[0], usuarioContext?.isAuthContext[1]] as [boolean, Dispatch<SetStateAction<boolean>>];
+    const context = useContext(UsuarioContext);
+    const [isAuth, setIsAuth] = [context?.isAuthContext[0], context?.isAuthContext[1]] as [boolean, Dispatch<SetStateAction<boolean>>];
 
     return [isAuth, setIsAuth];
-
 }
